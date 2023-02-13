2b2481582c345646ba338a4d83ebc33e3f4ec002
Ana Castedo <anaccastedo@gmail.com>
2023-02-13 12:52:30 +0000

    useImperativeHandle to focus inside a component using external refs

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,4 @@
-import React, {useContext, useEffect, useReducer, useState} from 'react';
-
+import React, {useContext, useEffect, useReducer, useRef, useState} from 'react';
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
@@ -34,6 +33,8 @@ const Login = () => {
   // const [enteredPassword, setEnteredPassword] = useState('');
   // const [passwordIsValid, setPasswordIsValid] = useState();
   const authCtx=useContext(AuthContext);
+  const emailInputRef=useRef({});
+  const passwordInputRef=useRef({});
 
   const [formIsValid, setFormIsValid] = useState(false);
 
@@ -73,16 +74,23 @@ const {isValid: emailIsValid}=emailState;
 
   const submitHandler = (event) => {
     event.preventDefault();
-    authCtx.onLogin(emailState.value, passwordState.value);
+    if(formIsValid){
+      authCtx.onLogin(emailState.value, passwordState.value);
+    }else if(!emailIsValid){
+      emailInputRef.current.focus();
+    }else{
+      passwordInputRef.current.focus();
+    }
+
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <Input id='email' label='E-Mail' type='email' isValid={emailIsValid} value={emailState.value} onChange={emailChangeHandler} onBlur={validateEmailHandler}/>
-        <Input id='password' label='Password' type='password' isValid={passwordIsValid} value={passwordState.value} onChange={passwordChangeHandler} onBlur={validatePasswordHandler}/>
+        <Input ref={emailInputRef} id='email' label='E-Mail' type='email' isValid={emailIsValid} value={emailState.value} onChange={emailChangeHandler} onBlur={validateEmailHandler}/>
+        <Input ref={passwordInputRef} id='password' label='Password' type='password' isValid={passwordIsValid} value={passwordState.value} onChange={passwordChangeHandler} onBlur={validatePasswordHandler}/>
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn}>
             Login
           </Button>
         </div>
